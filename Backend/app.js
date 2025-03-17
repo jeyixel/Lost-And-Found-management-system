b06cffe50f97b414a1c10b52cca558c4e659d12c
jeyixel <jjayawardene610@gmail.com>
2025-03-17 16:38:28 +0530

    Successfull connection to mongoDB

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,18 @@
 console.log('bye World');
+// pass = 8sOnzmeBJSiPekJr
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// middleware
+app.use("/",(req, res, next) => {
+    res.send("It is working bn");
+});
+
+mongoose.connect("mongodb+srv://jcj2:8sOnzmeBJSiPekJr@cluster1.thoez.mongodb.net/")
+.then(() => console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000); // localhost:5000 on browswer
+})
+.catch(err => console.log(err));
