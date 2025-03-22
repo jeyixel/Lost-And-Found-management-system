5a357bf76472ffbd8c6198b9dc01fd06d2a3cf29
jeyixel <jjayawardene610@gmail.com>
2025-03-22 16:01:28 +0530

    applied css styles for launch page

--- frontend/src/Components/LaunchingPage/Launchpage.js
+++ frontend/src/Components/LaunchingPage/Launchpage.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './launch.css';
 
 function Launchpage() {
   return (
-    <div>
-      <h1>Welcome to FindItFast</h1>
-      <p>A very own lost and found management system for students of sliit</p>
+    <div className='launch-body'>
+      <div className="welcome">
+        <h1>Welcome to FindItFast</h1>
+      </div>
 
-      <Link to="/login">login</Link>
-      <Link to="/createAccount">Create Account</Link>
+      <div className="para">
+        <p>A very own lost and found management system for students of sliit</p>
+      </div>
+
+      <div className="buttondeka">
+
+        <div className="button1">
+          <button className="button1-class">
+            <Link className='launchlink' to="/login">login</Link>
+          </button>
+        </div>
+
+        <div classNameName="button2">
+          <button className='button2-class'>
+            <Link className='launchlink' to="/createAccount">Create Account</Link>
+          </button>
+        </div>
+
+      </div>
     </div>
   )
 }
